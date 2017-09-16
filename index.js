6e0e3e3ce94a453c3a3998b49fcfb04902c74c06
Eyas Ranjous <eyas.ranjous@gmail.com>
2017-09-17 02:16:03 +0300

    use es6

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-/*!
- * sort-algorithms-js
- * index (main module object)
- * Copyright(c) 2015 Eyas Ranjous <eyas.ranjous@gmail.com>
- * MIT Licensed
- */
-
 'use strict';
 
+let sorterFactory = require('./lib/sorterFactory');
+
 module.exports = {
-    bubbleSort: require('./lib/bubbleSort'),
-    insertionSort: require('./lib/insertionSort'),
-    selectionSort: require('./lib/selectionSort'),
-    heapSort: require('./lib/heapSort'),
-    quickSort: require('./lib/quickSort'),
-    mergeSort: require('./lib/mergeSort')
+    bubbleSort: (list) => {
+        return sorterFactory.create('bubble', list).sort();
+    },
+    insertionSort: (list) => {
+        return sorterFactory.create('insertion', list).sort();
+    },
+    selectionSort: (list) => {
+        return sorterFactory.create('selection', list).sort();
+    },
+    heapSort: (list) => {
+        return sorterFactory.create('heap', list).sort();
+    },
+    quickSort: (list) => {
+        return sorterFactory.create('quick', list).sort();
+    },
+    mergeSort: (list) => {
+        return sorterFactory.create('merge', list).sort();
+    }
 };
