c09fc245dc76117c9ddbd532f2a331d3b9c4a582
Eyas Ranjous <eyas.ranjous@gmail.com>
2018-06-14 20:35:28 -0500

    refactor

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-'use strict';
+const bubbleSort = require('./lib/bubbleSort');
+const insertionSort = require('./lib/insertionSort');
+const selectionSort = require('./lib/selectionSort');
+const heapSort = require('./lib/heapSort');
+const quickSort = require('./lib/quickSort');
+const mergeSort = require('./lib/mergeSort');
 
-/**
- * sort-algorithms-js entry point
- */
-module.exports = require('./lib/sortAdapter');
+module.exports = {
+  bubbleSort,
+  insertionSort,
+  selectionSort,
+  heapSort,
+  quickSort,
+  mergeSort
+};
