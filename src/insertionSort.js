8d8cf1d6b79b6c5c138572d9aa783d3e2a0bfcea
Eyas Ranjous <eyas.ranjous@gmail.com>
2020-04-26 01:31:42 -0500

    insertionSort

---
+++ src/insertionSort.js
@@ -0,0 +1,32 @@
+/**
+ * sort-algorithms-js/insertionSort
+ * @copyright 2018 Eyas Ranjous <eyas.ranjous@gmail.com>
+ * @license MIT
+ */
+
+const SortAlgorithm = require('./sortAlgorithm');
+
+/**
+ * @class InsertionSort
+ * @extends SortAlgorithm
+ */
+class InsertionSort extends SortAlgorithm {
+  /**
+   * @public
+   * @return {array}
+   */
+  sort() {
+    for (let i = 1; i < this._list.length; i += 1) {
+      let insertionIndex = i;
+      for (let j = i - 1; j >= 0; j -= 1) {
+        if (this._shouldSwap(j, insertionIndex)) {
+          this._swap(j, insertionIndex);
+          insertionIndex = j;
+        }
+      }
+    }
+    return this._list;
+  }
+}
+
+module.exports = InsertionSort;
---
+++ test/insertionSort.test.js
@@ -0,0 +1,37 @@
+const { expect } = require('chai');
+const data = require('./fixtures/data.json');
+const InsertionSort = require('../src/insertionSort');
+
+describe('.sort()', () => {
+  it('should throw an error when list is not an array', () => {
+    expect(() => new InsertionSort('test').sort()).to.throw(Error)
+      .and.to.have.property('message', 'Sort: invalid array');
+  });
+
+  it('sorts a list of numbers in ascending order', () => {
+    expect(new InsertionSort(data.numbers.t1.unsorted).sort())
+      .to.deep.equal(data.numbers.t1.sorted.asc);
+
+    expect(new InsertionSort(data.numbers.t2.unsorted).sort())
+      .to.deep.equal(data.numbers.t2.sorted.asc);
+  });
+
+  it('sorts a list of numbers in descending order', () => {
+    expect(new InsertionSort(data.numbers.t1.unsorted, (a, b) => (b - a)).sort())
+      .to.deep.equal(data.numbers.t1.sorted.desc);
+
+    expect(new InsertionSort(data.numbers.t2.unsorted, (a, b) => (b - a)).sort())
+      .to.deep.equal(data.numbers.t2.sorted.desc);
+  });
+
+  it('sorts a list of strings in ascending order', () => {
+    expect(new InsertionSort(data.strings.t1.unsorted).sort())
+      .to.deep.equal(data.strings.t1.sorted.asc);
+  });
+
+  it('sorts a list of strings in descending order', () => {
+    const compare = (a, b) => (a > b ? -1 : 1);
+    expect(new InsertionSort(data.strings.t1.unsorted, compare).sort())
+      .to.deep.equal(data.strings.t1.sorted.desc);    
+  });
+});
